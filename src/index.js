76edb856e68411b94f5144066179c6e9ea560e3c
Pytniza <ip.web.so@gmail.com>
2018-10-07 19:24:40 +0300

    pass test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let simple = [], exp = [], zeros = [];
+  let counter = 0, b = base, summ = 0, num = number, mult, ex = 2;
+  for (let i = 2; i <= b; i++){
+    if (b%i == 0) {
+      simple.push(i);
+    }
+    while (b%i == 0){
+      b /= i;
+      counter++;
+    }
+
+    if (counter > 0) {
+            exp.push(counter);
+    }
+    counter = 0;
+  }
+  for (var j = 0; j < simple.length; j++){
+    mult = simple[j];
+    while (num/mult > 1){
+      summ += Math.floor(num/mult);
+      mult = Math.pow(simple[j], ex);
+      ex++;
+    }
+    zeros.push(summ);
+    summ = 0;
+    ex = 2;
+  }
+  for (let k = 0; k < zeros.length; k++){
+    zeros[k] = Math.floor(zeros[k]/exp[k]);
+  }
+
+zeros.sort(function(a, b){return a-b;});
+return zeros[0];
 }
